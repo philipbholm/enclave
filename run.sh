73cc480e174ca0f39c1705273491cb8d87f79ca9
Philip Holm <philipbaltzersen@gmail.com>
2024-09-11 12:25:25 +0200

    Terminate enclave after 3 sec in run script

--- run.sh
+++ run.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-docker build -t docker-image . 
-nitro-cli build-enclave --docker-uri docker-image --output-file enclave.eif
+docker build -t server . 
+nitro-cli build-enclave --docker-uri server --output-file enclave.eif
 nitro-cli run-enclave --eif-path enclave.eif --cpu-count 2 --memory 1024 --debug-mode
 nitro-cli console --enclave-id $(nitro-cli describe-enclaves | jq -r '.[0].EnclaveID')
+sleep 3
+
+nitro-cli terminate-enclave --all
