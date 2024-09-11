df7c969a53eccd8efbae5eae86fd23d727bb3132
Philip Holm <philipbaltzersen@gmail.com>
2024-09-11 12:30:16 +0200

    Kill console process

--- run.sh
+++ run.sh
@@ -3,6 +3,7 @@ docker build -t server .
 nitro-cli build-enclave --docker-uri server --output-file enclave.eif
 nitro-cli run-enclave --eif-path enclave.eif --cpu-count 2 --memory 1024 --debug-mode
 nitro-cli console --enclave-id $(nitro-cli describe-enclaves | jq -r '.[0].EnclaveID')
-sleep 3
+sleep 5
+kill -SIGINT $$
 
 nitro-cli terminate-enclave --all
