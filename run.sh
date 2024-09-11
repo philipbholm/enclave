2601d9f951d6cfcad5c95c9e89220744465cf2c2
Philip Holm <philipbaltzersen@gmail.com>
2024-09-11 10:43:57 +0200

    Rename docker image

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-docker build -t enclave-image . 
-nitro-cli build-enclave --docker-uri enclave-image --output-file enclave.eif
+docker build -t docker-image . 
+nitro-cli build-enclave --docker-uri docker-image --output-file enclave.eif
 nitro-cli run-enclave --eif-path enclave.eif --cpu-count 2 --memory 1024 --debug-mode
 nitro-cli console --enclave-id $(nitro-cli describe-enclaves | jq -r '.[0].EnclaveID')
