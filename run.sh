74d5d31505eae2dcf3fbaa1bf3c6a461ba77c409
Philip Holm <philipbaltzersen@gmail.com>
2024-09-11 14:54:20 +0200

    Remove terminate step from script

--- run.sh
+++ run.sh
@@ -3,7 +3,3 @@ docker build -t server .
 nitro-cli build-enclave --docker-uri server --output-file enclave.eif
 nitro-cli run-enclave --eif-path enclave.eif --cpu-count 2 --memory 1024 --debug-mode
 nitro-cli console --enclave-id $(nitro-cli describe-enclaves | jq -r '.[0].EnclaveID')
-sleep 5
-kill -SIGINT $$
-
-nitro-cli terminate-enclave --all
